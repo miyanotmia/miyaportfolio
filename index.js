aee60b94cd60db47afe8c3d0ac67c491bcf1ba14
Miya Simms <miysimms@outlook.com>
2025-09-04 14:59:26 -0400

    UI Revamp version 0.5 Complete... Version 1 to include all projects. Ready to deploy

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-function myFunction() {
+function darkMode() {
     document.body.classList.toggle("dark-mode");
     document.querySelector(`.navbar`).classList.toggle("navbar-dark-mode");
     document.querySelector(`#profileborder`).classList.toggle("profile-dark-mode");
     document.querySelector(`#profilepic`).classList.toggle("pic-dark-mode");
     document.querySelector(`#aboutme`).classList.toggle("aboutme-dark-mode");
     document.querySelector(`.mywork`).classList.toggle("mywork-dark-mode");
-}
+     document.querySelector(`.header`).classList.toggle("header-dark-mode");}
+
+
+     const text="Miya Simms";
+     const span=document.getElementById("typewrite");
+     let i=0;
+function typeWriter(){
+        if (i < text.length){
+            span.textContent += text.charAt(i); i++;
+            setTimeout(typeWriter,150);
+        }
+     }
+     span.textContent = ""
+     typeWriter();
+     
